a23832681d445273e1cb1ec05a09d9e9465b32a4
Whitney Burton <whitneyburton9@gmail.com>
2019-02-05 16:27:37 -0700

    Add CreateAccountPrompt styling and details to message

--- src/components/CreateAccountPrompt/CreateAccountPrompt.js
+++ src/components/CreateAccountPrompt/CreateAccountPrompt.js
@@ -7,11 +7,13 @@ const CreateAccountPrompt = (props) => {
   setPopup(true)
 
   return <div className='CreateAccountPrompt'>
-    <p>Would you like to create an account?</p>
+    <p className='create-account-prompt'>In order to <span role='img' aria-label='star emoji'>⭐</span> your favorite movies, you need an account. Would you like to create one?</p>
+    <hr/>
     <Link to='/create-user' onClick={() => setShouldPromptLogin(false)}>YES</Link>
     <button className='no-btn' onClick={() => { setShouldPromptLogin(false); setPopup(false) }}>NO</button>
+    <hr/>    
+    <p className='account-notice'>(If you already have an account, you can login from the home page!)</p>
   </div >
 }
-
 
 export default CreateAccountPrompt
