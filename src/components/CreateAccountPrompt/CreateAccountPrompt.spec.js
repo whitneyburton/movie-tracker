d824ad1d42f8838140d17b8391759fb5537a44be
Justin <justin@baal-software.com>
2019-02-04 20:27:15 -0700

    Add createAccount test

--- src/components/CreateAccountPrompt/CreateAccountPrompt.spec.js
+++ src/components/CreateAccountPrompt/CreateAccountPrompt.spec.js
@@ -3,21 +3,38 @@ import CreateAccountPrompt from './CreateAccountPrompt'
 import { shallow } from 'enzyme'
 
 describe('CreateAccountPrompt', () => {
-  const setShouldPromptLoginMock = jest.fn()
-  const setPopupMock = jest.fn()
-  const wrapper = shallow(<CreateAccountPrompt
-    setShouldPromptLogin={setShouldPromptLoginMock}
-    setPopup={setPopupMock}
-  />)
-  
+  let setShouldPromptLoginMock
+  let setPopupMock
+  let wrapper
+  beforeEach(() => {
+    window.fetch = jest.fn()
+    setShouldPromptLoginMock = jest.fn()
+    setPopupMock = jest.fn()
+    wrapper = shallow(<CreateAccountPrompt
+      setShouldPromptLogin={setShouldPromptLoginMock}
+      setPopup={setPopupMock}
+    />)
+  })
+
+
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('should call setShouldPromptLogin when component mounts', () => {
+    expect(setPopupMock).toHaveBeenCalledTimes(1)
+  })
+
   it('should call setPopup and setShouldPromptLogin when clicked', () => {
-    wrapper.find('.test').simulate('click')
+    wrapper.find('button').simulate('click')
     expect(setPopupMock).toHaveBeenCalledTimes(2)
+    expect(setShouldPromptLoginMock).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call setPopup and setShouldPromptLogin when clicked', () => {
+    wrapper.find('Link').simulate('click')
+    expect(setPopupMock).toHaveBeenCalledTimes(1)
   })
 
 })
